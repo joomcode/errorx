fc849e595bf0876beb83ea0fe4083c58b1b173b9
p.ivanov <p.ivanov@joom.it>
2021-02-18 11:28:52 +0300

    godoc

--- error_113.go
+++ error_113.go
@@ -4,12 +4,13 @@ package errorx
 
 import "reflect"
 
-// todo godoc
-// NB: Call to errors.As() converts any type of errorx error to any other type, therefore such calls may break semantics.
-// Note than calls to errors.Is() do not suffer from the same issue.
-// todo add errorx.As() ?
-// todo make another type for passing into errors.As()?
+// todo add errorx.As()?
+// todo make another 'go error type' for passing into errors.As()?
 // todo when fixed, add tests for wrap/decorate etc.
+// As checks if target is of the same type as current error and, if true, sets target to this error value.
+// NB: Call to errors.As() converts any type of errorx error to any other type,
+// therefore such calls are currently unsafe for errorx errors and will likely break semantics.
+// Note than calls to errors.Is() do not suffer from the same issue.
 func (e *Error) As(target interface{}) bool {
 	targetError, ok := target.(*error)
 	if !ok {
@@ -25,18 +26,17 @@ func (e *Error) As(target interface{}) bool {
 	return true
 }
 
-// todo godoc
+// Is returns true if and only if target is errorx error that passes errorx type check against current error.
+// This behaviour is exactly the same as that of IsOfType().
+// See also: errors.Is()
 func (e *Error) Is(target error) bool {
 	typedTarget := Cast(target)
-	if typedTarget == nil {
-		return false
-	}
-
-	return e.IsOfType(typedTarget.Type())
+	return typedTarget != nil && IsOfType(e, typedTarget.Type())
 }
 
-// todo godoc
-// If e.Unwrap() returns a non-nil error w, then we say that e wraps w.
+// From errors package: if e.Unwrap() returns a non-nil error w, then we say that e wraps w.
+// Unwrap returns cause of current error in case it is wrapped transparently, nil otherwise.
+// See also: errors.Unwrap()
 func (e *Error) Unwrap() error {
 	if e.cause != nil && e.transparent {
 		return e.cause
